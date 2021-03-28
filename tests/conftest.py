e0aadc7c385918eaa78f2215ea0164302603fdf4
Josh Karpel <josh.karpel@gmail.com>
2021-03-28 13:59:28 -0500

    getting thing set up

---
+++ tests/conftest.py
@@ -0,0 +1,20 @@
+from functools import partial
+from typing import Callable, List
+
+import pytest
+from click.testing import Result
+from typer.testing import CliRunner
+
+from orate.main import app
+
+CLI = Callable[[List[str]], Result]
+
+
+@pytest.fixture
+def runner() -> CliRunner:
+    return CliRunner()
+
+
+@pytest.fixture
+def cli(runner: CliRunner) -> CLI:
+    return partial(runner.invoke, app)

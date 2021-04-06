aedd11a3eee800bc3cb8d3ae4139eb1cc0b18c60
JoshKarpel <josh.karpel@gmail.com>
2021-04-05 22:50:29 -0500

    add some basic docs

--- docs/conf.py
+++ docs/conf.py
@@ -1,22 +1,3 @@
-# Configuration file for the Sphinx documentation builder.
-#
-# This file only contains a selection of the most common options. For a full
-# list see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
-
-# -- Path setup --------------------------------------------------------------
-
-# If extensions (or modules to document with autodoc) are in another directory,
-# add these directories to sys.path here. If the directory is relative to the
-# documentation root, use os.path.abspath to make it absolute, like shown here.
-#
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
-
-
-# -- Project information -----------------------------------------------------
-
 import sphinx_rtd_theme
 
 import spiel
@@ -27,10 +8,26 @@ author = "Josh Karpel"
 release = spiel.constants.__version__
 
 
-extensions = ["sphinx.ext.autodoc", "sphinx.ext.napoleon", "sphinx_rtd_theme"]
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",
+    "sphinx_rtd_theme",
+]
 
 templates_path = ["_templates"]
 html_static_path = ["_static"]
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 html_theme = "sphinx_rtd_theme"
+
+# sphinx-autodoc
+autoclass_content = "both"
+autodoc_member_order = "bysource"
+autodoc_default_options = {
+    "members": True,
+    "undoc-members": True,
+}
+autodoc_typehints = "signature"
+
+# sphinx-autodoc-typehints
+set_type_checking_flag = True
---
+++ docs/reference.rst
@@ -0,0 +1,23 @@
+Reference
+=========
+
+.. py:currentmodule:: spiel
+
+Decks and Slides
+----------------
+
+.. autoclass:: Deck
+
+.. autoclass:: Slide
+
+
+Extra Renderables
+-----------------
+
+.. autoclass:: Image
+
+
+Triggers
+--------
+
+.. autoclass:: Triggers

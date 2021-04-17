cfbcb669e56c56caf55b92521013ad716b678bb7
Josh Karpel <josh.karpel@gmail.com>
2021-04-17 16:28:59 -0500

    Container image improvements (#17)

--- docker/test.sh
+++ docker/test.sh
@@ -1,6 +1,8 @@
-set -xeu
+#!/usr/bin/env bash
+
+set -eux
 
 TAG="spiel:test"
 
 docker build -f docker/Dockerfile -t "$TAG" .
-docker run -it --rm "$TAG" spiel version
+docker run -it --rm "$TAG" $@

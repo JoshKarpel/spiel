96fd63b53988a7ff0d9ac20d74dfdadc632ab37d
JoshKarpel <josh.karpel@gmail.com>
2021-05-20 23:02:48 -0500

    add some caches for Plot and Image to increase rendering speed

--- spiel/image.py
+++ spiel/image.py
@@ -1,9 +1,10 @@
 from __future__ import annotations
 
 from dataclasses import dataclass
+from functools import lru_cache
 from math import floor
 from pathlib import Path
-from typing import Iterable, NamedTuple
+from typing import Iterable, List, NamedTuple, Tuple
 
 from PIL import Image as Img
 from rich.color import Color
@@ -18,6 +19,30 @@ from .utils import chunks
 class ImageSize(NamedTuple):
     width: int
     height: int
+
+
+@lru_cache(maxsize=2 ** 8)
+def _pixels_to_ansi(
+    pixels: Tuple[Tuple[int, int, int], ...], size: ImageSize, justify: JustifyMethod
+) -> Text:
+    rows = [
+        [
+            Text(
+                "▀",
+                Style(
+                    color=Color.from_rgb(*top),
+                    bgcolor=Color.from_rgb(*bottom) if bottom else None,
+                ),
+            )
+            # ... produce one row of text, using upper-half-blocks for the top image row and the background for the bottom image row
+            for top, bottom in zip(top_row, bottom_row)
+        ]
+        # for each pair of rows in the image...
+        for top_row, bottom_row in chunks(
+            chunks(pixels, size.width), 2, fill_value=[None] * size.width
+        )
+    ]
+    return Text("\n", justify=justify).join(Text("").join(row) for row in rows)
 
 
 @dataclass(frozen=True)
@@ -51,25 +76,6 @@ class Image:
     def __rich_console__(self, console: Console, options: ConsoleOptions) -> Iterable[Segment]:
         size = self._determine_size(options)
         resized = self._resize(size)
-
-        rows = [
-            [
-                Text(
-                    "▀",
-                    Style(
-                        color=Color.from_rgb(*top),
-                        bgcolor=Color.from_rgb(*bottom) if bottom else None,
-                    ),
-                )
-                # ... produce one row of text, using upper-half-blocks for the top image row and the background for the bottom image row
-                for top, bottom in zip(top_row, bottom_row)
-            ]
-            # for each pair of rows in the image...
-            for top_row, bottom_row in chunks(
-                chunks(resized.getdata(), size.width), 2, fill_value=[None] * size.width
-            )
-        ]
-
-        text = Text("\n", justify=self.justify).join(Text("").join(row) for row in rows)
-
+        pixels = tuple(resized.getdata())
+        text = _pixels_to_ansi(pixels, size, justify=self.justify)
         yield from text.__rich_console__(console, options)
--- spiel/plot.py
+++ spiel/plot.py
@@ -1,6 +1,7 @@
+import pickle
 import re
 from functools import lru_cache
-from typing import Any, Iterable, Sequence, Union
+from typing import Any, Iterable, List, Sequence, Union
 
 import numpy as np
 import uniplot
@@ -18,42 +19,54 @@ Plottable = Union[
 
 RE_ANSI_ESCAPE = re.compile(r"(\x1B(?:[@-Z\\-_]|\[[0-?]*[ -/]*[@-~]))")
 
-COLOR_MAP = {
-    Fore.RED: "red",
-    Fore.GREEN: "green",
-    Fore.BLUE: "blue",
-    Fore.CYAN: "cyan",
-    Fore.YELLOW: "yellow",
-    Fore.MAGENTA: "magenta",
-    Fore.BLACK: "black",
-    Fore.WHITE: "white",
+ANSI_COLOR_TO_STYLE = {
+    CStyle.RESET_ALL: Style.null(),
+    Fore.RED: Style(color="red"),
+    Fore.GREEN: Style(color="green"),
+    Fore.BLUE: Style(color="blue"),
+    Fore.CYAN: Style(color="cyan"),
+    Fore.YELLOW: Style(color="yellow"),
+    Fore.MAGENTA: Style(color="magenta"),
+    Fore.BLACK: Style(color="black"),
+    Fore.WHITE: Style(color="white"),
 }
 
 
+@lru_cache(maxsize=2 ** 8)
+def _ansi_to_text(s: str) -> Text:
+    pieces = []
+    tmp = ""
+    null_style = Style.null()
+    style = null_style
+    for char in RE_ANSI_ESCAPE.split(s):
+        if char in ANSI_COLOR_TO_STYLE:
+            pieces.append(Text(tmp, style=style))
+            style = ANSI_COLOR_TO_STYLE[char]
+            tmp = ""
+        else:
+            tmp += char
+
+    # catch leftovers
+    pieces.append(Text(tmp, style=style))
+
+    return Text("", no_wrap=True).join(pieces)
+
+
+@lru_cache(maxsize=2 ** 8)
+def _make_plot(plot_args: bytes) -> List[str]:
+    # This is kind of ugly, but we pickle the args before passing them as an easy
+    # way to make them hashable. This helps a lot for performance on static plots,
+    # and doesn't have toooooo much impact on dynamic plots.
+    plot_args = pickle.loads(plot_args)
+    return uniplot.plot_to_string(**plot_args)
+
+
 class Plot:
-    def __init__(self, **plot_args: Any) -> None:
+    def __init__(
+        self,
+        **plot_args: Any,
+    ) -> None:
         self.plot_args = plot_args
-
-    def _ansi_to_text(self, s: str) -> Text:
-        pieces = []
-        tmp = ""
-        style = Style.null()
-        for char in RE_ANSI_ESCAPE.split(s):
-            if char == CStyle.RESET_ALL:
-                pieces.append(Text(tmp, style=style))
-                style = Style.null()
-                tmp = ""
-            elif char in COLOR_MAP:
-                pieces.append(Text(tmp, style=style))
-                style = Style(color=COLOR_MAP[char])
-                tmp = ""
-            else:
-                tmp += char
-
-        # catch leftovers
-        pieces.append(Text(tmp, style=style))
-
-        return Text("", no_wrap=True).join(pieces)
 
     def __rich_console__(self, console: Console, options: ConsoleOptions) -> Iterable[Segment]:
         if self.plot_args.get("height") is None and options.height is None:
@@ -72,8 +85,6 @@ class Plot:
             ),
         }
 
-        plot = "\n".join(uniplot.plot_to_string(**plot_args))
+        plot = "\n".join(_make_plot(pickle.dumps(plot_args)))
 
-        text = self._ansi_to_text(plot)
-
-        yield from text.__rich_console__(console, options)
+        yield from _ansi_to_text(plot).__rich_console__(console, options)

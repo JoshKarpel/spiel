e8a8d0cf85a2864a716f1084197e090fc1eae1d3
Josh Karpel <josh.karpel@gmail.com>
2022-10-23 20:43:10 -0500

    footer

--- spiel/app.py
+++ spiel/app.py
@@ -1,17 +1,22 @@
 from __future__ import annotations
 
+import datetime
 from dataclasses import dataclass, field
 from typing import Callable
 
-from rich.console import RenderableType
+from rich.console import Group, RenderableType
 from rich.panel import Panel
+from rich.rule import Rule
 from rich.style import Style
+from rich.table import Column, Table
+from rich.text import Text
 from textual.app import App, ComposeResult
 from textual.binding import Binding
 from textual.reactive import reactive
 from textual.screen import Screen
 from textual.widget import Widget
-from textual.widgets import Footer, Header
+
+from spiel.utils import clamp, drop_nones, filter_join
 
 
 @dataclass
@@ -25,6 +30,9 @@ class Slide:
 class Deck:
     name: str
     slides: list[slide] = field(default_factory=list)
+
+    def __len__(self) -> int:
+        return len(self.slides)
 
     def slide(
         self,
@@ -76,8 +84,13 @@ class DeckViewSlideWidget(Widget):
         self.set_interval(1 / 10, self.refresh)
 
 
+class SlideView(Screen):
+    def compose(self) -> ComposeResult:
+        yield SlideWidget()
+        yield Footer()
+
+
 class DeckView(Screen):
-    #: Bindings for the help screen.
     BINDINGS = [
         ("escape,enter,down", "pop_screen", "Close"),
     ]
@@ -85,6 +98,63 @@ class DeckView(Screen):
     def compose(self) -> ComposeResult:
         for idx, slide in enumerate(self.app.deck.slides):
             yield DeckViewSlideWidget(slide=slide, slide_idx=idx)
+
+        yield Footer()
+
+
+class Footer(Widget):
+    DEFAULT_CSS = """
+    Footer {
+        color: $text;
+        dock: bottom;
+        height: 2;
+    }
+    """
+
+    @property
+    def longest_slide_number_length(self) -> int:
+        num_slides = len(self.app.deck)
+        return len(str(num_slides))
+
+    def render(self) -> RenderableType:
+        grid = Table.grid(
+            *drop_nones(
+                Column(
+                    style=Style(dim=True),
+                    justify="left",
+                ),
+                Column(
+                    style=Style(bold=True),
+                    justify="center",
+                ),
+                Column(
+                    style=Style(dim=True),
+                    justify="right",
+                ),
+                Column(
+                    style=Style(dim=True),
+                    justify="right",
+                ),
+            ),
+            expand=True,
+            padding=1,
+        )
+        grid.add_row(
+            *drop_nones(
+                Text(
+                    filter_join(
+                        " | ",
+                        [self.app.deck.name, self.app.current_slide.title],
+                    )
+                ),
+                self.app.message,
+                datetime.datetime.now().strftime("%Y-%m-%d %I:%M %p"),
+                Text(
+                    f"[{self.app.slide_idx + 1:>0{self.longest_slide_number_length}d} / {len(self.app.deck)}]"
+                ),
+            )
+        )
+        return Group(Rule(style=Style(dim=True)), grid)
 
 
 class SpielApp(App):
@@ -99,35 +169,32 @@ class SpielApp(App):
     SCREENS = {"deck": DeckView()}
 
     slide_idx = reactive(0)
+    message = reactive("")
 
     def compose(self) -> ComposeResult:
         """Create child widgets for the app."""
-        yield Header()
         yield Footer()
-        yield SlideWidget()
 
     def on_mount(self):
         from demo.demo import deck
 
         self.deck = deck
 
-        w = self.query_one(SlideWidget)
-        w.slide_idx = 0
-
-        self.log(w)
-        w.focus()
+        self.push_screen(SlideView())
 
     def action_toggle_dark(self) -> None:
         """An action to toggle dark mode."""
         self.dark = not self.dark
 
     def action_next_slide(self) -> None:
-        if self.slide_idx < len(self.deck.slides) - 1:
-            self.slide_idx += 1
+        self.slide_idx = clamp(self.slide_idx + 1, 0, len(self.deck) - 1)
 
     def action_prev_slide(self) -> None:
-        if self.slide_idx > 0:
-            self.slide_idx -= 1
+        self.slide_idx = clamp(self.slide_idx - 1, 0, len(self.deck) - 1)
+
+    @property
+    def current_slide(self) -> Slide:
+        return self.deck.slides[self.slide_idx]
 
 
 if __name__ == "__main__":

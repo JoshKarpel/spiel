4efd672764a7d6aceb7ad7d58c8355ed771b709c
Josh Karpel <josh.karpel@gmail.com>
2023-01-03 23:00:33 -0600

    fix typo

--- docs/examples/triggers_animation.py
+++ docs/examples/triggers_animation.py
@@ -16,15 +16,15 @@ def animate(triggers: Triggers) -> RenderableType:
     space = "  "
     bar_length = 5
 
-    spaces_before_face = min(floor(triggers.time_since_first_trigger), bar_length)
-    spaces_after_face = bar_length - spaces_before_face
+    spaces_before_bang = min(floor(triggers.time_since_first_trigger), bar_length)
+    spaces_after_bang = bar_length - spaces_before_bang
 
-    bar = (space * spaces_before_face) + bang + (space * spaces_after_face)
+    bar = (space * spaces_before_bang) + bang + (space * spaces_after_bang)
 
     return Align(
         Group(
             Align.center(Text(f"{triggers.time_since_first_trigger=:.1f}")),
-            Align.center(Text(f"{spaces_before_face=} | {spaces_after_face=}")),
+            Align.center(Text(f"{spaces_before_bang=} | {spaces_after_bang=}")),
             Align.center(Panel(Text(bar), expand=False, height=3)),
         ),
         align="center",
